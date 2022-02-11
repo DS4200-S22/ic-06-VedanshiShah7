66f43f62f4f77860f3a745f68a256163ba09c455
VedanshiShah7 <56982656+VedanshiShah7@users.noreply.github.com>
2022-02-10 20:19:57 -0500

    assignment

--- main.js
+++ main.js
@@ -10,5 +10,17 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
+    randNum = Math.random();
+    let newRandNum = 'Your new random number is: ' + randNum + '!';
+    let randomNum = document.getElementById('random_num');
+    randomNum.innerHTML = newRandNum;
+
     console.log("Button 1 was clicked"); 
+}
+
+function button2Clicked() {
+    // if color
+    let color = document.getElementsByClassId('circle_color').style;
+    // color.innerHTML = color2; 
+    console.log(color);
 }
